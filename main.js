d123eb476e0bb44d092ab7ea80d53bf96a2db3e2
Nirmal <Nirmal@admin.com>
2025-03-15 14:28:35 +0400

    port

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ app.use((err, req, res, next) => {
 });
 
 //start server
-var port=3000;
+const port = process.env.PORT || 3000;
 app.listen('0.0.0.0',port,function(){ //to set open ip addres
     console.log(`Server is listening on port ${port}`);
 });
